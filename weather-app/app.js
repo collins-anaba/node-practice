f1cd4cd851b2fe3ff4c9332cb195628e6f2038e1
Collins Evulukwu <siranaba@Collinss-MacBook-Air.local>
2020-01-17 15:59:58 -0600

    forecastjs added

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,6 @@
 const request = require('request');
 const geocode = require('./Utils/geocode')
+const forecast = require('./Utils/forecast')
 
 // const url = 'https://api.darksky.net/forecast/14ca8beee7b5101972be43af6264878d/37.8267,-122.4233?'
 
@@ -7,22 +8,16 @@ const geocode = require('./Utils/geocode')
 // console.log( `${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability}% chance of rain `)
 // })
 
-// const url2 = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZGV0b3gyazIiLCJhIjoiY2s1ZTNweDhhMGJpbTNmbTRqZHJqNzJ2eSJ9.ri0DzXLKi9eieAbbfR7DrQ'
 
-// request({url: url2, json: true}, (error,response)=>{
-//     if (error){
-//         console.log('Unable to connect')
-//     } else if (response.body.features.length === 0 ){
-//          console.log('unable to locate')
-//     }else {    
-//     console.log(`${response.body.features[0].center[1]}, ${response.body.features[0].center[0]}`)
-// }
-// })
 
 
 
 geocode("Dallas", (error,data)=> {
 console.log('Error', error)
 console.log('Data', data)
+})
 
+forecast (-96.7969, 32.7763, (error,data)=> {
+console.log('Error', error)
+console.log('Data', data)
 })
